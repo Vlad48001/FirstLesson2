9a687599d77c462c93074de5c130385c7805f186
Vlad480 <zapolskijvladislav089@gmail.com>
2023-11-07 19:41:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int temp = scanner.nextInt();
+
+        System.out.println(temp * 2);
+
+        System.out.printf("Hello and welcome!");
+
+    }g
+}
